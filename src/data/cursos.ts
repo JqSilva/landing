14a0e222376784616367c6c5dbc5987da97c1367
joaquin <joaquinsilvagvg@gmail.com>
2025-06-17 18:12:17 -0400

    feat: Agregar nuevas imágenes y optimizar carga en secciones About, Clients, Contact, Curso y Team; implementar lazy loading

--- src/data/cursos.ts
+++ src/data/cursos.ts
@@ -1,3 +1,6 @@
+import imagen_servicio from '../assets/images/servicio.jpg';
+
+
 // src/data/cursos.ts
 export const cursos = [
   {
@@ -5,21 +8,21 @@ export const cursos = [
     descripcion: "Curso especializado en trabajo seguro con instalaciones eléctricas activas.",
     modalidad: "Presencial",
     duracion: "40 horas",
-    imagen: "/images/servicio.jpg",
+    imagen: imagen_servicio,
   },
   {
     titulo: "Seguridad Eléctrica",
     descripcion: "Capacitación en normativas y buenas prácticas de seguridad eléctrica.",
     modalidad: "Presencial",
     duracion: "20 horas",
-    imagen: "/images/servicio.jpg",
+    imagen: imagen_servicio,
   },
   {
     titulo: "Eficiencia Energética",
     descripcion: "Curso sobre técnicas y estrategias para mejorar la eficiencia energética.",
     modalidad: "Online",
     duracion: "25 horas",
-    imagen: "/images/servicio.jpg",
+    imagen: imagen_servicio,
   },
   
 ];
