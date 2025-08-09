b7fdf0436c914494f16e0757bd7babe1caf62c74
joaquin <joaquinsilvagvg@gmail.com>
2025-08-08 20:32:58 -0400

    🚀 Deploy automático

---
+++ swiper.js
@@ -0,0 +1,15 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+
+  //pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
